b3f1c3cf9377e1c70f9a1c8c9a7ea7d3c9721598
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-19 14:07:11 +0100

    Add margins to activity_sign_up.xml

--- app/src/main/java/com/hu/sandoruszkai/androiddesign/SignUpActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/SignUpActivity.kt
@@ -1,14 +1,17 @@
 package com.hu.sandoruszkai.androiddesign
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity : AppCompatActivity(){
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
     }
 
 }
