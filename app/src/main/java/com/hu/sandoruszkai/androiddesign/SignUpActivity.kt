874b3c43932bd15c6dbb282068cf83a1806678b2
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-19 12:15:30 +0100

    Add layout elements for SignUpActivity

--- app/src/main/java/com/hu/sandoruszkai/androiddesign/SignUpActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/SignUpActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
-class LoginActivity : AppCompatActivity(){
+class SignUpActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
     }
+
 }
--- app/src/main/java/com/hu/sandoruszkai/androiddesign/SplashActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/SplashActivity.kt
@@ -1,40 +1,14 @@
 package com.hu.sandoruszkai.androiddesign
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.squareup.picasso.Picasso
-import jp.wasabeef.picasso.transformations.BlurTransformation
-import kotlinx.android.synthetic.main.activity_splash.*
-
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        hideSystemUI()
-        blurBackgroud()
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
     }
-
-    private fun hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                // Set the content to appear under the system bars so that the
-                // content doesn't resize when the system bars hide and show.
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
-    }
-
-    private fun blurBackgroud() {
-        Picasso.with(this).load(R.drawable.splash_background)
-            .transform(BlurTransformation(this,25,1)).into(ivBackground)
-    }
-
 }
