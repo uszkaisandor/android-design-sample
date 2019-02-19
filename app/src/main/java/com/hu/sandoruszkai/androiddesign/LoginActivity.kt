d6e16a1f578a92ad9e91bc9dfbe2bfd228024160
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-19 17:37:40 +0100

    Add custom fonts

--- app/src/main/java/com/hu/sandoruszkai/androiddesign/LoginActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/LoginActivity.kt
@@ -1,15 +1,31 @@
 package com.hu.sandoruszkai.androiddesign
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class LoginActivity : AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         window.decorView.apply {
             systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
         }
+    }
+    private fun setUpListeners() {
+        btnLogIn.setOnClickListener {
+            startLoginActivity()
+        }
+        btnImAlreadyADev.setOnClickListener{
+            startLoginActivity()
+        }
+    }
+
+    private fun startLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/hu/sandoruszkai/androiddesign/SplashActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/SplashActivity.kt
@@ -8,6 +8,7 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
         val intent = Intent(this, SignUpActivity::class.java)
         startActivity(intent)
     }
