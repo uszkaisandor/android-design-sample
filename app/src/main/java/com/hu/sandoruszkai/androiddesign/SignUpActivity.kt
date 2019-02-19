d02dc04e89061a6e349bd0df202ff4902fa3255d
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-19 16:04:13 +0100

    Add activity_layout.xml layout resources

--- app/src/main/java/com/hu/sandoruszkai/androiddesign/SignUpActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/SignUpActivity.kt
@@ -1,11 +1,12 @@
 package com.hu.sandoruszkai.androiddesign
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity(){
+class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +14,20 @@ class SignUpActivity : AppCompatActivity(){
         window.decorView.apply {
             systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
         }
+        setUpListeners()
     }
 
+    private fun setUpListeners() {
+        btnCreateMyAccount.setOnClickListener {
+            startLoginActivity()
+        }
+        btnImAlreadyADev.setOnClickListener{
+            startLoginActivity()
+        }
+    }
+
+    private fun startLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
 }
