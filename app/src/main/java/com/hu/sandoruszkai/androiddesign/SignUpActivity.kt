330132ac9527f2b5db6bd90cf51722c4e2724efa
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-19 15:22:08 +0100

    Add background to activity_sign_up.xml

--- app/src/main/java/com/hu/sandoruszkai/androiddesign/SignUpActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/SignUpActivity.kt
@@ -3,6 +3,7 @@ package com.hu.sandoruszkai.androiddesign
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity(){
 
