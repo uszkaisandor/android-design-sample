b1d095d8e431dc9064c43d2591f1e6661fa41182
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-20 10:00:36 +0100

    Add ProfileActivity layout without the ViewPager

--- app/src/main/java/com/hu/sandoruszkai/androiddesign/ProfileActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/ProfileActivity.kt
@@ -1,11 +1,20 @@
 package com.hu.sandoruszkai.androiddesign
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
     }
 }
