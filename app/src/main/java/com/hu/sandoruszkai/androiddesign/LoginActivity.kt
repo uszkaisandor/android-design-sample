6558dbd56a72b9e936c998fd95291f2d3f79ebcd
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-20 09:59:26 +0100

    Make all activities fullscreen (immersive sticky)

--- app/src/main/java/com/hu/sandoruszkai/androiddesign/LoginActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/LoginActivity.kt
@@ -12,7 +12,12 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         window.decorView.apply {
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+            systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         }
         setUpListeners()
     }
--- app/src/main/java/com/hu/sandoruszkai/androiddesign/SignUpActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/SignUpActivity.kt
@@ -12,7 +12,12 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         window.decorView.apply {
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+            systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         }
         setUpListeners()
     }
