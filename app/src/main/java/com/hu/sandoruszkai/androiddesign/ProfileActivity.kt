4a2d0b05a5064ca2e85aee610d3041a957993b38
Sandor Uszkai <uszkai.sandor@codeyard.eu>
2019-02-21 11:58:57 +0100

    Add fragments

--- app/src/main/java/com/hu/sandoruszkai/androiddesign/ProfileActivity.kt
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/ProfileActivity.kt
@@ -1,9 +1,9 @@
 package com.hu.sandoruszkai.androiddesign
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_profile.*
 
 
@@ -11,6 +11,26 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        hideDefaultNavBar()
+        setNavBarListeners()
+        setFragment(ProfileFragment())
+    }
+
+    private fun setNavBarListeners() {
+        navbarBottom.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_search -> {
+                    setFragment(SearchFragment())
+                }
+                R.id.action_home -> {
+                    setFragment(ProfileFragment())
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+    }
+
+    private fun hideDefaultNavBar() {
         window.decorView.apply {
             systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
@@ -18,19 +38,12 @@ class ProfileActivity : AppCompatActivity() {
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         }
-        viewPager?.let {
-            val adapter = ViewPagerAdapter(supportFragmentManager, this)
-            viewPager.adapter = adapter
-        }
-        navbarBottom.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.action_search -> {
-                }
-                else -> {
-                }
-            }
-            return@setOnNavigationItemSelectedListener true
-        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.frameFragmentHolder, fragment)
+        ft.commit()
     }
 
 }
---
+++ app/src/main/java/com/hu/sandoruszkai/androiddesign/ProfileFragment.kt
@@ -0,0 +1,23 @@
+package com.hu.sandoruszkai.androiddesign
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_profile.*
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewPager?.let {
+            val adapter = ViewPagerAdapter(childFragmentManager, requireActivity())
+            viewPager.adapter = adapter
+        }
+    }
+}
